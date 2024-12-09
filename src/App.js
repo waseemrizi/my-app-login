2f608ad87337afb357b10371c948d22480bd1e02
waseemrizi <waseemrizi@outlook.com>
2024-12-10 04:00:47 +0500

    updating footer section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import LoginPage from './components/LoginPage/LoginPage';
+import Footer from './components/Footer/Footer';  // Import Footer component
 
 function App() {
     return (
         <div className="App">
             <LoginPage />
+            <Footer />  {/* Add the Footer component */}
         </div>
     );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css'; // Link to the CSS file for styling
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>All rights reserved &copy; 2024</p>
+        </div>
+    );
+};
+
+export default Footer;
